b900b2d432ebc96a3534ad3546cc6a09cdb113a8
Ivan Acosta-Rubio <ivan@bakedweb.net>
2013-09-26 12:09:48 -0500

    extracting the sharing module from Meals.mx

--- lib/project/motion-social.rb
+++ lib/project/motion-social.rb
@@ -1,6 +1,40 @@
-class Motion
-  class Social
+module MontionSocial
+  module Sharing
+    # Usage:
+    # include MotionSocial::Sharing
+    #
+    # define the following methods:
+    #
+    # def sharing_message
+    # def sharing_url
+    # def controller
+    #
+    def postToTwitter(sender)
+      if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter)
+        tweetSheet = SLComposeViewController.composeViewControllerForServiceType(SLServiceTypeTwitter)
+        tweetSheet.setInitialText(sharing_message)
+        url = NSURL.alloc.initWithString(sharing_url)
+        tweetSheet.addURL(url)
+        controller.presentViewController(tweetSheet,animated:true, completion:nil)
+      else
+        error_message_for("Twitter")
+      end
+    end
 
+    def postToFacebook(sender)
+      if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook)
+        sheet = SLComposeViewController.composeViewControllerForServiceType(SLServiceTypeFacebook)
+        sheet.setInitialText(sharing_message)
+        url = NSURL.alloc.initWithString(sharing_url)
+        sheet.addURL(url)
+        controller.presentViewController(sheet,animated:true,completion:nil)        
+      else
+        error_message_for("Facebook")
+      end
+    end
+
+    def error_message_for(service)
+      App.alert("You don't have #{service} configured. Please go to your phone's settings and configure it.")
+    end
   end
 end
-
